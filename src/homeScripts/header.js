605d4b362b4fe1e083f06fd81cc661dfba5b76d6
conztantin <conztantinsixten@gmail.com>
2025-05-06 13:32:14 +0200

    most recent fix of logo

--- src/homeScripts/header.js
+++ src/homeScripts/header.js
@@ -1,10 +1,11 @@
 import newsify_logo from '/images/newsify_logo1.png'
+
 export default function header() {
     let headerElm = document.createElement('header');
     headerElm.innerHTML = `
     <nav class="flex-row center margin-top-small margin-bottom-small">
         <figure class="logo__home">
-            <img class="authlogo__img" src="${newsify_logo}" alt="">
+            <img src="${newsify_logo}" class="authlogo__img" alt="">
         </figure>
         <p class="logo__text">Newsify</p>
     </nav>
